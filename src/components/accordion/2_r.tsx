4aa3626bf01eae41d05d4c2dc3397241425d400c
wootaiklee <fined000@naver.com>
2025-11-08 18:28:00 +0900

    feat: accordion2

---
+++ src/components/accordion/2_r.tsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import cx from "./cx";
+import { accordionData } from "./data";
+
+const AccordionItem = ({
+  id,
+  title,
+  description,
+  current,
+  toggle,
+}: (typeof accordionData)[number] & {
+  current: boolean;
+  toggle: () => void;
+}) => {
+  return (
+    <li key={id} className={cx("item2", { current })}>
+      <div className={cx("tab")} onClick={toggle}>
+        {title}
+      </div>
+      <p className={cx("description")}>{description}</p>
+    </li>
+  );
+};
+
+const Accordion2 = () => {
+  const [currentId, setCurrentId] = useState<string | null>(
+    accordionData[0].id
+  );
+
+  const toggleItem = (id: string) => () => {
+    setCurrentId((prev) => (prev === id ? null : id));
+  };
+
+  return (
+    <>
+      <h3>#2. React</h3>
+      <ul className={cx("container")}>
+        {accordionData.map((d) => (
+          <AccordionItem
+            key={d.id}
+            {...d}
+            current={currentId === d.id}
+            toggle={toggleItem(d.id)}
+          />
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default Accordion2;
--- src/components/accordion/index.tsx
+++ src/components/accordion/index.tsx
@@ -1,4 +1,5 @@
 import Accordion1 from "./1_r";
+import Accordion2 from "./2_r";
 import cx from "./cx";
 
 const Accordion = () => {
@@ -6,6 +7,7 @@ const Accordion = () => {
     <div className={cx("Accordion")}>
       <h1>아코디언1</h1>
       <Accordion1 />
+      <Accordion2 />
     </div>
   );
 };
