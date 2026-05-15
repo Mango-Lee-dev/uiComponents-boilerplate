7527145cd7549556d02e5cb21061447baae7240a
wootaiklee <fined000@naver.com>
2026-05-15 23:15:00 +0900

    feat: enhance LazyImage and LazyLoad1 components with built-in lazy loading support and improved loading state management

--- src/components/lazyLoading/1_r/index.tsx
+++ src/components/lazyLoading/1_r/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import cx from "../cx";
 import data from "../data";
 import useIntersectionObserver from "@/hooks/useIntersectionObserver";
@@ -17,34 +17,52 @@ const LazyImage = ({
   height: number;
 }) => {
   const imgRef = useRef<HTMLImageElement>(null);
+  const [loadded, setLoadded] = useState(false);
   const { entires, observeRef } = useIntersectionObserver(imgRef, ioOptions);
 
   useEffect(() => {
+    if ("loading" in HTMLImageElement.prototype) {
+      observeRef.current?.disconnect();
+      return;
+    }
+
     const isVisible = entires[0]?.isIntersecting;
     if (isVisible) {
-      imgRef.current!.src = src;
+      const onLoad = () => {
+        setLoadded(true);
+      };
+      imgRef.current!.addEventListener("load", onLoad);
+      imgRef.current!.setAttribute("src", src);
+      observeRef.current?.disconnect();
     }
   }, [src, entires]);
+
   return (
     <img
       ref={imgRef}
-      src=""
+      src={src}
       width={width}
       height={height}
       alt=""
-      className={cx("img", "lazy")}
+      className={cx("img", loadded ? "" : "lazy")}
+      loading="lazy"
     />
   );
 };
 
+const builtInLazySupported = "loading" in HTMLImageElement.prototype;
+
 const LazyLoad1 = () => {
+  const Component = builtInLazySupported
+    ? (props: any) => <img {...props} />
+    : LazyImage;
   return (
     <>
       <h3>
         React<sub>#1</sub>
       </h3>
       {data.map((url, index) => (
-        <LazyImage src={url} key={`${index}-${url}`} width={600} height={320} />
+        <Component src={url} key={`${index}-${url}`} width={600} height={320} />
       ))}
     </>
   );
