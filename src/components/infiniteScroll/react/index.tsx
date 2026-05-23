aa7a40f0fd81a4db8587e2f01b68915e24446f11
wootaiklee <fined000@naver.com>
2026-05-23 20:12:06 +0900

    feat: implement InfiniteScroll component with initial layout and styling, including spinner animation and sample data

---
+++ src/components/infiniteScroll/react/index.tsx
@@ -0,0 +1,11 @@
+const InfiniteScrollR = () => {
+  return (
+    <>
+      <h2>무한 스크롤</h2>
+      <h3>#1. React - scroll로 감지하는 방식</h3>
+      <ul></ul>
+    </>
+  );
+};
+
+export default InfiniteScrollR;
