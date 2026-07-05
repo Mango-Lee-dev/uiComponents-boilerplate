25bebed31759baf02f25b29fd28681b4e63222a7
wootaiklee <fined000@naver.com>
2026-07-05 14:38:53 +0900

    feat: update ScrollBox component to manage button states based on intersection observer entries, enhancing navigation functionality

--- src/components/scrollBox/react/index.tsx
+++ src/components/scrollBox/react/index.tsx
@@ -1,7 +1,15 @@
-import { useCallback, useRef } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import cx from "../cx";
 import data from "../data";
 import { LazyImage } from "@/components/lazyLoading/1_r";
+import useIntersectionObserver from "@/hooks/useIntersectionObserverV2";
+
+type Direction = "prev" | "next";
+
+const DefaultButtonState: { prev: boolean; next: boolean } = {
+  prev: true,
+  next: true,
+};
 
 const Item = ({
   id,
@@ -19,7 +27,12 @@ const Item = ({
 );
 
 const ScrollBox = () => {
+  const [buttonEnabled, setButtonEnabled] = useState<{
+    prev: boolean;
+    next: boolean;
+  }>(DefaultButtonState);
   const watcherRef = useRef<(HTMLLIElement | null)[]>([]);
+  const { entries: watcherEntries } = useIntersectionObserver(watcherRef);
 
   const move = useCallback((direction: "prev" | "next") => {
     const elem: HTMLElement = document.createElement("div");
@@ -30,6 +43,20 @@ const ScrollBox = () => {
     });
     elem.remove();
   }, []);
+
+  useEffect(() => {
+    setButtonEnabled(() => {
+      // watcher가 화면에 보이면 해당 방향의 끝에 도달한 것이므로 버튼을 끈다.
+      const newState = { ...DefaultButtonState };
+      watcherEntries.forEach((e) => {
+        const direction = (e.target as HTMLLIElement).dataset
+          .direction as Direction;
+        newState[direction] = false;
+      });
+      return newState;
+    });
+  }, [watcherEntries]);
+
   return (
     <>
       <h3>ScrollBox - React</h3>
@@ -40,6 +67,7 @@ const ScrollBox = () => {
             ref={(r) => {
               watcherRef.current[0] = r;
             }}
+            data-direction="prev"
           />
           {data.map((item) => (
             <li key={item.id} className={cx("item")}>
@@ -51,10 +79,17 @@ const ScrollBox = () => {
             ref={(r) => {
               watcherRef.current[1] = r;
             }}
+            data-direction="next"
           />
         </ul>
-        <button className={cx("nav-button", "prev", { on: true })} />
-        <button className={cx("nav-button", "next", { on: true })} />
+        <button
+          className={cx("nav-button", "prev", { on: buttonEnabled.prev })}
+          onClick={() => move("prev")}
+        />
+        <button
+          className={cx("nav-button", "next", { on: buttonEnabled.next })}
+          onClick={() => move("next")}
+        />
       </div>
     </>
   );
